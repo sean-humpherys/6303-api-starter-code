5e2c98a7b0c58d4bce56106009ac1da67c507e88
Sean Humpherys <shumpherys@wtamu.edu>
2022-10-19 18:08:25 -0500

    Added starter code

---
+++ get_countries.py
@@ -0,0 +1,14 @@
+"""
+Task: Get and display a list of countries from an API server at
+https://www.thesportsdb.com/api/v1/json/2/all_countries.php 
+
+API Documentation at https://www.thesportsdb.com/api.php 
+
+The hardest part with API data is figuring out how to access the data
+becuase the data is usually nested inside lists and dictionaries inside
+of other lists or other dicionaries. 
+Every API server is different in how it embeds data inside 
+lists and dictionaries. You just need to peel back every layer of a list 
+or a dictionary until you get the data you want. 
+"""
+# Enter your code below this line
